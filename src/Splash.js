56e72a66b71e403391d7c9a298d2581e38f668df
Ravin Thambapillai <rthambapillai@palantir.com>
2019-04-20 17:49:27 +0100

    add onSignIn function

--- src/Splash.js
+++ src/Splash.js
@@ -3,8 +3,20 @@ import './App.css';
  
 export class SplashPage extends Component {
   render () {
+
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      }
+
     return (
         <div className="wrapper">
+        <div class="top-menu">
+            <div class="g-signin2" data-onsuccess="onSignIn"></div>
+        </div>
             <div className="hero-image">
                     <div className="hero-text">
                     <h1>How Britain Thinks</h1>
@@ -16,7 +28,6 @@ export class SplashPage extends Component {
                 <p><img src={require("./images/propogate-security.svg")} />Securely express your opinion on the issues that matter to you</p>
                 <p><img src={require("./images/protect-production.svg")} />Get insights into which areas and groups of Britons think what</p>
                 <p><img src={require("./images/track-versions.svg")} />Get organized! Connect with others trying to make the world better!</p>
-                <div class="g-signin2" data-onsuccess="onSignIn"></div>
             </div>
         </div>
     );
